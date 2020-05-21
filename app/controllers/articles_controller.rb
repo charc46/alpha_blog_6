44c168493a2cf97352888f23bd9ecf1d55fb441a
Charles Crane <charlie.crane46@gmail.com>
2020-05-21 19:48:49 +0400

    Copy of previous alpha-blog app, created with rails 6 instead

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  # The 'before_action' allows us to add our set_article method to our selected methods at the beginning of the controller, helping to prevent repeated code
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+  # Actions, each need a view file in the views/articles folder.
+  def show 
+  # Takes the article from the Article class and assigns it to the @article instance variable to be called in our show view
+  end
+
+  def index
+    # Takes all articles from the Article class and saves them to the @articles instance variable
+    @articles = Article.all
+  end
+
+  def new
+    # We have to initialize a new article instance in order for our page to load without a nilCLass error
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    # This enabled our new article to be posted to the articles table, we must whitelist the required fields using the permit method
+    @article = Article.new(article_params)
+    # We then save the article to the table
+    if @article.save
+      # If our article saves we display a success message
+      flash[:notice] = "Article was saved successfully"
+      # We then re-direct to the show article path so that we can see our new article
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private # Anything below the 'private' keyword is exclusive to this controller only
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
