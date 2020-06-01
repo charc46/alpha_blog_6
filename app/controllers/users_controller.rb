ddf8c21bb792f0fb20e14773e8f814b55d418cc7
Charles Crane <charlie.crane46@gmail.com>
2020-06-01 06:14:13 +0400

    Add users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @user = User.all
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
